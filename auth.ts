eb1a6a330c3caf7164c0c35c62ccaa21654df205
kdarley <39971882+kdarley@users.noreply.github.com>
2025-09-22 23:35:52 -0500

    modified auth.ts

--- auth.ts
+++ auth.ts
@@ -21,8 +21,8 @@ async function getUser(email: string): Promise<User | undefined> {
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
+  trustHost: process.env.NEXTAUTH_TRUST_HOST === 'true', 
   providers: [
     Credentials({
       async authorize(credentials) {
