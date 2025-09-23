c9baf8013396958f03198b29742dea665b19f8e9
kdarley <39971882+kdarley@users.noreply.github.com>
2025-09-23 17:00:03 -0500

    testing without db calls to ui

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,9 +7,9 @@ import { lusitana } from '@/app/ui/fonts';
 // import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
 import { Suspense } from 'react';
 import {
-  RevenueChartSkeleton,
-  LatestInvoicesSkeleton,
-  CardsSkeleton,
+  // RevenueChartSkeleton,
+  // LatestInvoicesSkeleton,
+  // CardsSkeleton,
 } from '@/app/ui/skeletons';
 
 export const revalidate = 0;
@@ -24,14 +24,14 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
+          {/* <CardWrapper /> */}
         </Suspense>      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
+          {/* <RevenueChart /> */}
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
+          {/* <LatestInvoices /> */}
         </Suspense>
       </div>
     </main>
