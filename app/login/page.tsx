2d26c6e0afd69631ad832b43a29b06caf4da38d5
kdarley <39971882+kdarley@users.noreply.github.com>
2025-10-15 21:15:52 -0500

    added logos to working pages, fixed some styling inconsistencies e.g. spacing.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,17 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
+// import Image from 'next/image';
+import DarleyLogo from '@/app/ui/darley-logo';
 export const revalidate = 0;
  
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-12">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-24">
+          <div className="w-48 text-white md:w-64">
+            <DarleyLogo />
           </div>
         </div>
         <Suspense>
